702480dc1c0a50b6e77be7b9d60d2a0e9b214ba8
MuniKiranCh <125656756+MuniKiranCh@users.noreply.github.com>
2024-12-17 15:09:17 +0530

    added backend

---
+++ backend/src/routes/user.ts
@@ -0,0 +1,65 @@
+import { hash, compare } from "bcrypt-ts";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { Hono } from "hono";
+import { sign } from "hono/jwt";
+
+export const userRouter = new Hono<{
+    Bindings: {
+        DATABASE_URL: string;
+        JWT_SECRET: string;
+    }
+}>();
+
+userRouter.post('/signup', async (c) => {
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env?.DATABASE_URL,
+    }).$extends(withAccelerate());
+  
+    const body = await c.req.json();
+
+    const saltRounds=10;
+    const hashedPassword = await hash(body.password, saltRounds);
+  
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: hashedPassword,
+      },
+    });
+  
+    const token = await sign({ id: user.id }, c.env.JWT_SECRET)
+  
+    return c.json({
+      jwt: token
+    })
+})
+
+userRouter.post('/signin', async (c) => {
+    const prisma = new PrismaClient({
+    //@ts-ignore
+        datasourceUrl: c.env?.DATABASE_URL	,
+    }).$extends(withAccelerate());
+
+    const body = await c.req.json();
+    const user = await prisma.user.findUnique({
+        where: {
+            email: body.email,
+        }
+    });
+
+    if (!user) {
+        c.status(403);
+        return c.json({ error: "user not found" });
+    }
+
+    const match = await compare(body.password, user.password);
+
+    if (!match) {
+        c.status(403);
+        return c.json({ error: "Invalid Credentials" });
+    }
+
+    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+    return c.json({ jwt });
+})
